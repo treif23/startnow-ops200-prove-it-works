f96f5b6ed8cdfc228ec9ec8315eb0b20153e4f31
Tommy <treif23@github.com>
2018-05-03 13:42:42 -0700

    Update 5/3/18

--- test/e2e/app.spec.js
+++ test/e2e/app.spec.js
@@ -51,4 +51,18 @@ describe('End to End Tests', () => {
             .evaluate(() => document.querySelector('input[name=loanTerm]'))
             .then(input => expect(input).to.exist)
     });
+    it('should correctly calculate mortgage', () =>
+        pageObject
+            .wait()
+            .type('input[name=principal]', 300000)
+            .type('input[name=interestRate]', 3.75)
+            .type('input[name=loanTerm]', 30)
+            .select('select[name=period]', 12)
+            .click('button#calculate')
+            .wait('#output')
+            .evaluate(() => document.querySelector('#output').innerText)
+            .then((outputText) => {
+                expect(outputText).to.equal('$1389.35');
+            })
+    ).timeout(6500);
 })
